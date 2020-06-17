69304f4a5c1fc857ef61fcc7e33f318eff0da8ed
Bruno Lin <bruno27.lin@gmail.com>
2020-06-17 18:20:03 -0400

    more edits

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
-
+from flask import Flask, request
 
 app = Flask(__name__)
 
 ##dataset example
 armor = {"base":690,"shot1":690,"shot2": 700,"ss":1300}
-
+mobiles = {"armor":{"base":690,"shot1":690,"shot2": 700,"ss":1300},
+            "mage":{"base":690,"shot1":690,"shot2": 700,"ss":1300}}
 delay1 = 0
 delay2 = 0
 mobile_a= ''
@@ -17,9 +17,10 @@ def index():
 
 ## When the user clicks "start: on the app this function will initialize the variables needed
 ## such as the two mobiles that the user picked
+## {mobile_a: armor, mobile_b= mage}
 @app.route('/match')
 def init_match():
-    ## mobile_a = armor
+
     return "Ready to start"
 
 ## when the user clicks reset this function will clear all variables
@@ -29,6 +30,6 @@ def reset_match():
     return "match succesfully reset"
 
 ## when the user clicks submit this function will update the delay for the input mobile
-@app.route('/update/delay')
-
-    return 'succesfully updated delay'
+# @app.route('/update/delay')
+#
+# return 'succesfully updated delay'
--- frontend/src/delay.js
+++ frontend/src/delay.js
@@ -1,111 +1,64 @@
 import React, { useState } from 'react';
 import Navigation from './header'
-import {Container,Image,Figure,Row,Col,Button,ButtonToolbar,ButtonGroup,Jumbotron,Dropdown,DropdownButton,Table,Toast,Card,InputGroup,FormControl,Spinner} from 'react-bootstrap';
+import {Container,Image,Figure,Row,Col,Button,Jumbotron,Dropdown,DropdownButton,Table,Toast,InputGroup,FormControl,Spinner,ToggleButton} from 'react-bootstrap';
 import hyper10 from './images/random-1.png'
 import dual from './images/dual.png'
 import mobiles from './data'
 
 
-function MobileCard() {
-
-  return (
-    <>
-    <Jumbotron style={{backgroundColor:"#1c1f24"}}>
-    <h1><span className="delay-text">0</span></h1>
-
-    <DropdownButton id="dropdown-basic-button" title="Choose Mobile">
-      {mobiles.map((mobile,idx) => {
-        return(
-        <>
-        <Dropdown.Item key={idx}>{mobile.name}</Dropdown.Item>
-        </>
-      )}
-    )}
-    </DropdownButton>
-
-    <Figure>
-      <Figure.Image
-        width={150}
-        height={160}
-        alt="img"
-        src={mobiles[0].img}
-        rounded
-      />
-      <Figure.Caption>
-      </Figure.Caption>
-    </Figure>
-
-
-    <Row md={3} sm={3} lg={3}>
-        <Col>
-            <Button variant="primary">
-            1
-            <br/>
-            <span style= {{fontSize: "10px"}}>+770</span>
-            </Button>
-        </Col>
-
-        <Col>
-            <Button variant="primary">2
-            <br/>
-            <span style= {{fontSize: "10px"}}>+960</span>
-            </Button>
-        </Col>
-
-        <Col>
-            <Button variant="warning">
-            SS
-            <br/>
-            <span style= {{fontSize: "10px"}}>+1320</span>
-            </Button>
-        </Col>
-    </Row>
-
-        <br/>
-        <Row>
-          {[0,1,2].map((data,idx) => {
-            return(
-            <>
-            <Col
-            key={idx}>
-            <Button variant="danger">
-            <h6>+600</h6>
-              <Image src={dual} rounded/>
-            </Button>
-            </Col>
-            </>
-          )}
-        )}
-        </Row>
-
-    <br />
-
-    <Row>
-      <InputGroup>
-        <FormControl
-          placeholder="Max 20 seconds"
-          aria-label="time"
-          aria-describedby="basic-addon2"
-        />
-        <InputGroup.Text>seconds</InputGroup.Text>
-    </InputGroup>
-    </Row>
-
-    <br />
-
-    <Row>
-    <br />
-    <Col></Col>
-    <Col><Button variant="success">Submit</Button></Col>
-    <Col></Col>
-    </Row>
-    </Jumbotron>
-    </>
-  )
-};
-
 function DelayPage() {
-  const [currentPlayer,setPlayer] = useState("B");
+  const [currentPlayer,setPlayer] = useState();
+  const [mobileA,setA] = useState();
+  const [mobileB,setB] = useState();
+  const [itemValue,setItem] = useState([]);
+  const [shotValueA, setshotValueA] = useState('1');
+  const [shotValueB, setshotValueB] = useState('1');
+
+  const shotRef = {
+    1:"shot1",
+    2:"shot2",
+    3:"ss"
+  };
+
+  function findMobileB(e) {
+    return (e.name===mobileB)
+  };
+
+  function findMobileA(e) {
+    return (e.name===mobileA)
+  };
+
+  function toggleItem(val) {
+    setItem(val)
+  };
+
+  function handleSubmitA() {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({"currentPlayer": currentPlayer,  "shot_type": shotRef["shotValueA"],"secs_to_shoot": 10,"item": 1} )
+    }
+    fetch('api/',requestOptions)
+  };
+
+  function handleSubmitB() {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({"currentPlayer": currentPlayer,  "shot_type": shotRef["shotValueB"],"secs_to_shoot": 10,"item": 1} )
+    }
+    fetch('api/',requestOptions)
+  };
+
+  function initMatch() {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({"mobileA": mobileA , "mobileB": mobileB } )
+    };
+    fetch('api',requestOptions).then((res)=>res.json()).then((data) => setPlayer(data.currentPlayer))
+  };
+
   return (
     <div className="App">
       <Navigation />
@@ -113,34 +66,219 @@ function DelayPage() {
 
         <Container>
         <Row>
-        {currentPlayer==='A' ?<Spinner animation="grow" variant="danger" />: <></>}
-          <Col>
-          <h1 className="delay-text">Side A</h1>
-            <MobileCard />
-          </Col>
+        {currentPlayer==='mobileA' ?<Spinner animation="grow" variant="danger" />: <></>}
 
           <Col>
+            <h1 className="delay-text">Side A</h1>
+
+            <Jumbotron style={{backgroundColor:"#1c1f24"}}>
+              <h1><span className="delay-text">0</span></h1>
+                <DropdownButton id="dropdown-1" title={mobileA ? mobileA : "Choose Mobile"}>
+                  {mobiles.map((mobile) => {
+                    return(
+                    <>
+                    <Dropdown.Item key={mobile.name} as="button" onClick={() =>setA(mobile.name)}>{mobile.name}</Dropdown.Item>
+                    </>
+                  )}
+                )}
+                </DropdownButton>
+
+
+            {mobileA ?
+            <Figure>
+              <Figure.Image
+                width={150}
+                height={160}
+                alt="img"
+                src={mobiles[mobiles.findIndex((e) => findMobileA(e))].img}
+                rounded
+              />
+              <Figure.Caption>
+              </Figure.Caption>
+            </Figure> : <><br/></>
+          }
+
+              {/* Shot Type ToggleButton */}
+              <Row md={3} sm={3} lg={3}>
+              {[{ name: '1', value: '1' ,variant:"primary"},
+              { name: '2', value: '2',variant:"primary" },
+              { name: 'SS', value: '3' ,variant:"warning"}].map((data,idx) => (
+                  <Col>
+                      <ToggleButton
+                      key={idx}
+                      type="radio"
+                      variant={data.variant}
+                      value={data.value}
+                      checked={shotValueA===data.value}
+                      onChange={(e) => setshotValueA(e.currentTarget.value)}>
+                      <br/>
+                      {data.name}
+                      <br/>
+                      <span style= {{fontSize: "10px"}}>+770</span>
+                      </ToggleButton>
+                  </Col>
+              ))}
+              </Row>
+
+              <br/>
+
+              {/* Dual Item Buttons */}
+              <Row>
+                {[0,1,2].map((data,idx) => {
+                  return(
+                  <>
+                  <Col
+                  key={idx}>
+                  <Button variant="danger">
+                  <h6>+600</h6>
+                    <Image src={dual} rounded/>
+                  </Button>
+                  </Col>
+                  </>
+                )}
+              )}
+              </Row>
+
+              <br />
+
+              {/* Seconds input */}
+              <Row>
+                <InputGroup>
+                  <FormControl
+                    placeholder="Max 20 seconds"
+                    aria-label="time"
+                    aria-describedby="basic-addon2"
+                  />
+                  <InputGroup.Text>seconds</InputGroup.Text>
+              </InputGroup>
+              </Row>
+
+              <br />
+              {/* Submit button*/}
+              <Row>
+              <br />
+                <Col></Col>
+                <Col>{currentPlayer==="mobileA" ? <Button variant="success" onClick={handleSubmitA}>Submit</Button>: <Button variant="success" disabled>Submit</Button>}</Col>
+                <Col></Col>
+              </Row>
+          </Jumbotron>
+          </Col>
+
+          {/* VS Column */}
+          <Col>
           <h1 className="delay-text">VS</h1>
-          <Button variant="success" size="lg">Start</Button>
+          {(mobileB && mobileA) ? <Button variant="success" size="lg" onClick={initMatch}>Start</Button>:<Button variant="success" size="lg" disabled>Start</Button>}
           <Button variant="secondary" size="lg">Reset</Button>
+
           <br />
           <br />
+           {/*Notification Window */}
           <Toast
           style={{
           backgroundColor:"#e2ecff"}}>
             <Toast.Header>
               <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-              <strong className="mr-auto">Shot Tip</strong>
+              <strong className="mr-auto">Shot Tip: {mobileA}</strong>
               <small>Turn 4</small>
             </Toast.Header>
             <Toast.Body style={{color:"#1c1f24"}}>Fire Shot 1 in 5 seconds and you'll get to go again!</Toast.Body>
           </Toast>
           </Col>
 
-          {currentPlayer==='B' ?<Spinner animation="grow" variant="danger" />: <></>}
+          {/* Side B Column */}
+
+          {currentPlayer==='mobileB' ?<Spinner animation="grow" variant="danger" />: <></>}
           <Col>
           <h1 className="delay-text">Side B</h1>
-            <MobileCard />
+
+          <Jumbotron style={{backgroundColor:"#1c1f24"}}>
+          <h1><span className="delay-text">0</span></h1>
+
+          <DropdownButton id="dropdown-2" title={mobileB ? mobileB : "Choose Mobile"}>
+            {mobiles.map((mobile,idx) => {
+              return(
+              <>
+              <Dropdown.Item key={idx} as="button" onClick={() =>setB(mobile.name)}>{mobile.name}</Dropdown.Item>
+              </>
+            )}
+          )}
+          </DropdownButton>
+
+          {mobileB ?
+          <Figure>
+            <Figure.Image
+              width={150}
+              height={160}
+              alt="img"
+              src={mobiles[mobiles.findIndex((e) => findMobileB(e))].img}
+              rounded
+            />
+            <Figure.Caption>
+            </Figure.Caption>
+          </Figure> : <><br/></>
+        }
+
+
+          <Row md={3} sm={3} lg={3}>
+          {[{ name: '1', value: '1' ,variant:"primary"},
+          { name: '2', value: '2',variant:"primary" },
+          { name: 'SS', value: '3' ,variant:"warning"}].map((data,idx) => (
+              <Col>
+                  <ToggleButton
+                  key={idx}
+                  type="radio"
+                  variant={data.variant}
+                  value={data.value}
+                  checked={shotValueB===data.value}
+                  onChange={(e) => setshotValueB(e.currentTarget.value)}>
+                  <br/>
+                  {data.name}
+                  <br/>
+                  <span style= {{fontSize: "10px"}}>+770</span>
+                  </ToggleButton>
+              </Col>
+          ))}
+          </Row>
+
+              <br/>
+              <Row>
+                {[0,1,2].map((data,idx) => {
+                  return(
+                  <>
+                  <Col
+                  key={idx}>
+                  <Button variant="danger">
+                  <h6>+600</h6>
+                    <Image src={dual} rounded/>
+                  </Button>
+                  </Col>
+                  </>
+                )}
+              )}
+              </Row>
+
+          <br />
+
+          <Row>
+            <InputGroup>
+              <FormControl
+                placeholder="Max 20 seconds"
+                aria-label="time"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Text>seconds</InputGroup.Text>
+          </InputGroup>
+          </Row>
+
+          <br />
+
+          <Row>
+          <br />
+          <Col></Col>
+          <Col>{currentPlayer==="mobileB" ? <Button variant="success" onClick={handleSubmitB}>Submit</Button>: <Button variant="success" disabled>Submit</Button>}</Col>
+          <Col></Col>
+          </Row>
+          </Jumbotron>
           </Col>
 
         </Row>
@@ -183,5 +321,115 @@ function DelayPage() {
   );
 }
 
+// function MobileCard(props) {
+//
+//   return (
+//     <>
+//     <Jumbotron style={{backgroundColor:"#1c1f24"}}>
+//     <h1><span className="delay-text">0</span></h1>
+//     {props.mobileA ?
+//     <>
+//     <DropdownButton id="dropdown-basic-button" title={props.mobileA==="None" ? "Choose Mobile": props.mobileA}>
+//       {mobiles.map((mobile,idx) => {
+//         return(
+//         <>
+//         <Dropdown.Item key={idx} as="button" onClick={() =>props.setA(mobile.name)}>{mobile.name}</Dropdown.Item>
+//         </>
+//       )}
+//     )}
+//     </DropdownButton>
+//     </> :
+//     <>
+//     <DropdownButton id="dropdown-basic-button" title={props.mobileB==="None" ? "Choose Mobile": props.mobileB}>
+//       {mobiles.map((mobile,idx) => {
+//         return(
+//         <>
+//         <Dropdown.Item key={idx} as="button" onClick={() =>props.setB(mobile.name)}>{mobile.name}</Dropdown.Item>
+//         </>
+//       )}
+//     )}
+//     </DropdownButton>
+//     </>}
+//
+//     <Figure>
+//       <Figure.Image
+//         width={150}
+//         height={160}
+//         alt="img"
+//         src={mobiles[0].img}
+//         rounded
+//       />
+//       <Figure.Caption>
+//       </Figure.Caption>
+//     </Figure>
+//
+//
+//     <Row md={3} sm={3} lg={3}>
+//         <Col>
+//             <Button variant="primary">
+//             1
+//             <br/>
+//             <span style= {{fontSize: "10px"}}>+770</span>
+//             </Button>
+//         </Col>
+//
+//         <Col>
+//             <Button variant="primary">2
+//             <br/>
+//             <span style= {{fontSize: "10px"}}>+960</span>
+//             </Button>
+//         </Col>
+//
+//         <Col>
+//             <Button variant="warning">
+//             SS
+//             <br/>
+//             <span style= {{fontSize: "10px"}}>+1320</span>
+//             </Button>
+//         </Col>
+//     </Row>
+//
+//         <br/>
+//         <Row>
+//           {[0,1,2].map((data,idx) => {
+//             return(
+//             <>
+//             <Col
+//             key={idx}>
+//             <Button variant="danger">
+//             <h6>+600</h6>
+//               <Image src={dual} rounded/>
+//             </Button>
+//             </Col>
+//             </>
+//           )}
+//         )}
+//         </Row>
+//
+//     <br />
+//
+//     <Row>
+//       <InputGroup>
+//         <FormControl
+//           placeholder="Max 20 seconds"
+//           aria-label="time"
+//           aria-describedby="basic-addon2"
+//         />
+//         <InputGroup.Text>seconds</InputGroup.Text>
+//     </InputGroup>
+//     </Row>
+//
+//     <br />
+//
+//     <Row>
+//     <br />
+//     <Col></Col>
+//     <Col><Button variant="success">Submit</Button></Col>
+//     <Col></Col>
+//     </Row>
+//     </Jumbotron>
+//     </>
+//   )
+// };
 
 export default DelayPage;
