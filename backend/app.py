73976fbdf9dd1b09daba3249297589e042a75d60
bglin <brunolin@bu.edu>
2020-06-19 21:34:25 -0400

    added table logic, flask changes

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,21 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request,session
 import json
-import mobiledata from delaydata
+from delaydata import mobiledata
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 ##dataset example
 armor = {"base":690,"shot1":690,"shot2": 700,"ss":1300}
 
-delay1 = 0
-delay2 = 0
-mobile_a= ''
-mobile_b= ''
-item1del = 0
-item2del = 0
-
+## these are accesible globally but dont persist beween requests
+# delay1 = 0
+# delay2 = 0
+# mobile_a= ''
+# mobile_b= ''
+# item1del = 0
+# item2del = 0
 current_player =  ''
 
 @app.route('/')
@@ -22,51 +24,53 @@ def index():
 
 # When the app first loads, we want to hit this function to get all the mobiles delay info.
 ## The delay info will be used to populate the text on the buttons (i.e +600)
-@app.route('/mobile-info',methods = ['GET'])
+@app.route('/api/mobile-info',methods = ['GET'])
 def get_all_delays():
-    
+
     return (jsonify(data = mobiledata))
 
 ## When the user clicks "start" on the app this function will initialize the variables needed
 ## the function will return who goes first in the match (by setting the currentPlayer variable)
-@app.route('/match',methods = ['POST'])
+@app.route('/api/match',methods = ['POST'])
 def init_match():
     # accepts dictionary with keys mobileB,mobileA
     ##
     ## {mobileA: 'Armor',
     ##  mobileB: 'Aduka'}
-    
+
     ## this is the syntax for getting the incoming data
     ## the data is request.json
     ## the json functions convert the data to a dictionary
     incoming_data = json.loads(json.dumps(request.json))
-    
-    mobile_a = incoming_data["mobileA"]
-    mobile_b = incoming_data["mobileB"]
-    
+
+    session["mobile_a"] = incoming_data["mobileA"]
+    session["mobile_b"] = incoming_data["mobileB"]
+
     ##logic to set who goes first
     ## it will always be player A
     current_player = "mobileA"
-    
-    
+
+
     #here we are returning a dictionary with keys: message and currentPlayer
     return (jsonify(message= "Match Initiated!" ,currentPlayer= current_player))
 
 ## when the user clicks reset this function will clear all variables
-@app.route('/reset')
+@app.route('/api/reset', methods=["GET"])
 def reset_match():
-    delay1 = 0
-    delay2 = 0
-    reldelay1 = 0
-    reldelay2 = 0
+    session["mobile_a"]= ''
+    session["mobile_b"]= ''
+    session["delay1"] = 0
+    session["delay2"] = 0
+    # reldelay1 = 0
+    # reldelay2 = 0
     item1del = 0
     item2del = 0
     current_player = ''
-    
-    return "match succesfully reset"
+
+    return(jsonify({"mobileA":session["mobile_a"],"mobileB":session["mobile_b"],"delay1":session.get('delay1'),"delay2":session.get('delay2'),"currentPlayer":current_player}))
 
 ## when the user clicks "submit" this function will calculate the delay and end the player''s turn
-@app.route('/update/delay', methods = ['POST'])
+@app.route('/api/match/update', methods = ['POST'])
 def submit_turn():
     # accepts dictionary with keys
     ##
@@ -74,73 +78,62 @@ def submit_turn():
     ##  "shot_type": "1",
     ##  "secs_to_shoot": 10,
     ##  "item": 1} ## either 1 or 0
-    
+    print(armor["base"])
+
+    if 'delay1' not in session:
+        session["delay1"] = 0
+    if 'delay2' not in session:
+        session["delay2"] = 0
+
     incoming_data = json.loads(json.dumps(request.json))
-    
+
+
     ## logic to determine which side to update
     if incoming_data['currentPlayer']=='mobileA':
         type1 = incoming_data['shot_type']
         secs1 = incoming_data["secs_to_shoot"]
         item1 = incoming_data["item"]
-        if mobile_a == 'armor' and item1 == True:
+        if session["mobile_a"] == 'Armor' and item1 == True:
             item1del = 640
         elif item1 == True:
             item1del = 600
         else:
-            item1del ==0
-        delay1 = delaydata.mobiledata[mobile_a][type1] + \
-            secs1*delaydata.mobiledata[mobile_a][persec] + item1del
-        delay2 = delay2 - delay1
-    
-    
+            item1del= 0
+            print('hello')
+            print(session["mobile_a"])
+            print(type1)
+            print(secs1)
+            session["delay1"] = mobiledata[session["mobile_a"]][type1] + secs1*mobiledata[session["mobile_a"]]["persec"] + item1del
+            session["delay2"] =session["delay2"]- session["delay1"]
+
+
     ## logic to update delay for mob1 goes here
     ## im still confused on the relative delay and if we need to update delay for both mobiles after each turn
-    
+
     ##currentPlayer equals "mobileB"
     else:
         type2 = incoming_data['shot_type']
         secs2 = incoming_data["secs_to_shoot"]
         item2 = incoming_data["item"]
-        if mobile_b == 'armor' and item2 == True:
+        if session["mobile_b"] == 'Armor' and item2 == True:
             item2del = 640
         elif item2 == True:
             item2del = 600
         else:
-            item2del ==0
-        delay2 = delay2 + delaydata.mobiledata[mobile_b][type2] + \
-            secs2*delaydata.mobiledata[mobile_b][persec] + item2del
+            item2del= 0
+        session["delay2"] = session["delay2"] + mobiledata[session["mobile_b"]][type2] + secs2*mobiledata[session["mobile_b"]]["persec"] + item2del
 
-## logic to update delay for mob2 goes here
 
-# this is done rather explicitly since were only doing one item and im getting used to all this
-# so instead we'll be checking if incoming_data == mobileA
-#    if mobile_a == 'armor' and item1 == True:
-#        item1del = 640
-#    elif item1 == True:
-#        item1del = 600
-#    else:
-#        item1del ==0
-#
-#     if mobile_b == 'armor' and item2 == True:
-#        item2del = 640
-#    elif item2 == True:
-#        item2del = 600
-#    else:
-#        item2del ==0
-
-# delay = delay from before, shot based delay based on type, second based delay, item del
-#    delay1 = delay1 + delaydata.mobiledata[mobile_a][type1] + \
-#        secs1*delaydata.mobiledata[mobile_a][persec] + item1del
-#    delay2 = delay2 + delaydata.mobiledata[mobile_b][type2] + \
-#    secs2*delaydata.mobiledata[mobile_b][persec] + item2del
+    print(session["delay1"])
+    print(session["delay2"])
 
 # do i need to return anything
 ## You need to return the delay, and whose turn it is so the frontend can update the UI - Bruno
-if delay2 >= 0:
-    return (jsonify('mobileA')) ## not sure how we should ouput which turn it is
-    else
-        return (jsonify('mobileB')) ## not sure how we should ouput which turn it is
-
-return (jsonify(delay=delay1, delay2))
+    if session["delay2"] >= 0:
+        return (jsonify({"currentPlayer":'mobileA',"delay1":session.get('delay1'),"delay2":session.get('delay2')})) ## not sure how we should ouput which turn it is
+    else:
+        return (jsonify({"currentPlayer":'mobileB',"delay1":session.get('delay1'),"delay2":session.get('delay2')})) ## not sure how we should ouput which turn it is
 
 
+if __name__=="__main__":
+    app.run(host = '0.0.0.0')
--- frontend/src/delay.js
+++ frontend/src/delay.js
@@ -1,23 +1,109 @@
-import React, { useState } from 'react';
-import Navigation from './header'
-import {Container,Image,Figure,Row,Col,Button,Jumbotron,Dropdown,DropdownButton,Table,Toast,InputGroup,FormControl,Spinner,ToggleButton} from 'react-bootstrap';
+import React, { useState,useEffect } from 'react';
+import Navigation from './header';
+import InputRange from 'react-input-range';
+import "react-input-range/lib/css/index.css";
+import {Container,Image,Figure,Row,Col,Button,Jumbotron,Dropdown,DropdownButton,Table,Toast,FormControl,Spinner,ToggleButton,Alert} from 'react-bootstrap';
 import hyper10 from './images/random-1.png'
 import dual from './images/dual.png'
 import mobiles from './data'
 
 
 function DelayPage() {
+  const [delayData,setDelayData] = useState({});
   const [currentPlayer,setPlayer] = useState();
   const [mobileA,setA] = useState();
   const [mobileB,setB] = useState();
-  const [itemValue,setItem] = useState([]);
+  const [delayA,setDelayA] = useState(0);
+  const [delayB,setDelayB] = useState(0);
+  const [itemCountA,setItemCountA] = useState(3);
+  const [itemClickA,setItemClickA] = useState(false);
+  const [itemCountB,setItemCountB] = useState(3);
+  const [itemClickB,setItemClickB] = useState(false);
   const [shotValueA, setshotValueA] = useState('1');
   const [shotValueB, setshotValueB] = useState('1');
+  const [shotTimeA, setShotTimeA] = useState(0);
+  const [shotTimeB, setShotTimeB] = useState(0);
+  const [showAlert,setAlert] = useState(false);
 
   const shotRef = {
-    1:"shot1",
-    2:"shot2",
-    3:"ss"
+    '1':"shot1",
+    '2':"shot2",
+    '3':"ss"
+  };
+
+  const shotTypes =
+  [{ name: '1', value: '1' ,variant:"primary"},
+  { name: '2', value: '2',variant:"primary" },
+  { name: 'SS', value: '3' ,variant:"warning"}
+  ];
+
+  useEffect(() => {
+    fetch('/api/mobile-info').then(res => res.json()).then(mobiles => {
+    setDelayData(mobiles.data)
+  });
+  },[]);
+
+
+  function initMatch() {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({"mobileA": mobileA , "mobileB": mobileB } )
+    };
+    fetch('/api/match',requestOptions).then((res)=>res.json()).then((data) => setPlayer(data.currentPlayer))
+    setAlert(true)
+  };
+
+  function handleReset() {
+    fetch('/api/reset').then((res)=>res.json()).then((data) => {
+    setPlayer(data.currentPlayer)
+    setA(data.mobileA)
+    setB(data.mobileB)
+    setShotTimeA(0)
+    setShotTimeB(0)
+    setshotValueA('1')
+    setshotValueB('B')
+    setItemCountA(3)
+    setItemCountB(3)
+    setDelayA(0)
+    setDelayB(0)
+  })
+  };
+
+  function handleSubmitA() {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({"mobileA":mobileA,"mobileB":mobileB,"currentPlayer": currentPlayer, "shot_type": shotRef[shotValueA],"secs_to_shoot": shotTimeA,"item":itemClickA})
+    }
+    fetch('api/match/update',requestOptions).then((res)=>res.json()).then((data) => {
+    setPlayer(data.currentPlayer)
+    setDelayA(data.delay1)
+    setDelayB(data.delay2)
+  })
+
+    // reset item boolean
+    setItemClickA(false);
+
+    // start counter if SS was shot
+  };
+
+  function handleSubmitB() {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({"mobileA":mobileA,"mobileB":mobileB,"currentPlayer": currentPlayer,  "shot_type": shotRef[shotValueB],"secs_to_shoot": shotTimeB,"item": itemClickB})
+    }
+    fetch('/api/match/update',requestOptions).then((res)=>res.json()).then((data) => {
+    setPlayer(data.currentPlayer)
+    setDelayA(data.delay1)
+    setDelayB(data.delay2)
+  })
+
+    // reset item boolean
+    setItemClickB(false);
+
+    // start counter if SS was shot
   };
 
   function findMobileB(e) {
@@ -28,60 +114,48 @@ function DelayPage() {
     return (e.name===mobileA)
   };
 
-  function toggleItem(val) {
-    setItem(val)
+  function toggleItemA() {
+    setItemCountA(itemCountA-1);
+    setItemClickA(true);
   };
 
-  function handleSubmitA() {
-    const requestOptions = {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({"currentPlayer": currentPlayer,  "shot_type": shotRef["shotValueA"],"secs_to_shoot": 10,"item": 1} )
-    }
-    fetch('api/',requestOptions)
-  };
-
-  function handleSubmitB() {
-    const requestOptions = {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({"currentPlayer": currentPlayer,  "shot_type": shotRef["shotValueB"],"secs_to_shoot": 10,"item": 1} )
-    }
-    fetch('api/',requestOptions)
-  };
-
-  function initMatch() {
-    const requestOptions = {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({"mobileA": mobileA , "mobileB": mobileB } )
-    };
-    fetch('api',requestOptions).then((res)=>res.json()).then((data) => setPlayer(data.currentPlayer))
+  function toggleItemB() {
+    setItemCountB(itemCountB-1);
+    setItemClickB(true);
   };
 
   return (
     <div className="App">
       <Navigation />
       <div className="DelayPage">
-
         <Container>
+          <Alert show={showAlert} variant="success" onClose={() => setAlert(false)} dismissible>
+                The match has successfully started!
+          </Alert>
+
         <Row>
-        {currentPlayer==='mobileA' ?<Spinner animation="grow" variant="danger" />: <></>}
 
           <Col>
             <h1 className="delay-text">Side A</h1>
 
             <Jumbotron style={{backgroundColor:"#1c1f24"}}>
-              <h1><span className="delay-text">0</span></h1>
+            {currentPlayer==='mobileA' ?<Spinner animation="grow" variant="danger"/>: <></>}
+              <h1><span className="delay-text">{delayA}</span></h1>
+
+                {(mobileB && mobileA) ?
+                  <DropdownButton id="dropdown-1" title={mobileA ? mobileA : "Choose Mobile"}disabled>
+                  {mobiles.map((mobile,idx) => (
+                    <Dropdown.Item key={idx} as="button" onClick={() =>setA(mobile.name)}>{mobile.name}</Dropdown.Item>
+
+                ))}
+                </DropdownButton> :
                 <DropdownButton id="dropdown-1" title={mobileA ? mobileA : "Choose Mobile"}>
-                  {mobiles.map((mobile) => {
-                    return(
-                    <>
-                    <Dropdown.Item key={mobile.name} as="button" onClick={() =>setA(mobile.name)}>{mobile.name}</Dropdown.Item>
-                    </>
-                  )}
-                )}
-                </DropdownButton>
+                {mobiles.map((mobile,idx) => (
+                  <Dropdown.Item key={idx} as="button" onClick={() =>setA(mobile.name)}>{mobile.name}</Dropdown.Item>
+
+              ))}
+              </DropdownButton>
+                }
 
 
             {mobileA ?
@@ -100,17 +174,15 @@ function DelayPage() {
 
               {/* Shot Type ToggleButton */}
               <Row md={3} sm={3} lg={3}>
-              {[{ name: '1', value: '1' ,variant:"primary"},
-              { name: '2', value: '2',variant:"primary" },
-              { name: 'SS', value: '3' ,variant:"warning"}].map((data,idx) => (
-                  <Col>
+              {shotTypes.map((data,idx) => (
+                  <Col key={idx}>
                       <ToggleButton
-                      key={idx}
                       type="radio"
                       variant={data.variant}
                       value={data.value}
                       checked={shotValueA===data.value}
-                      onChange={(e) => setshotValueA(e.currentTarget.value)}>
+                      onChange={(e) => setshotValueA(e.currentTarget.value)}
+                      disabled={data.value ==='3' ? true : false}>
                       <br/>
                       {data.name}
                       <br/>
@@ -118,40 +190,40 @@ function DelayPage() {
                       </ToggleButton>
                   </Col>
               ))}
-              </Row>
+               </Row>
 
               <br/>
 
               {/* Dual Item Buttons */}
+              <Row><Col>{itemCountA} left</Col></Row>
               <Row>
-                {[0,1,2].map((data,idx) => {
-                  return(
-                  <>
-                  <Col
-                  key={idx}>
-                  <Button variant="danger">
+                  <Col>
+                  {itemCountA === 0 || itemClickA===true || shotValueA==='3' ?
+                  <Button variant="danger" disabled>
+                  <h6>+600</h6>
+                    <Image src={dual} rounded/>
+                  </Button> :
+
+                  <Button variant="danger" onClick={toggleItemA}>
                   <h6>+600</h6>
                     <Image src={dual} rounded/>
                   </Button>
+                }
                   </Col>
-                  </>
-                )}
-              )}
+
+
               </Row>
 
               <br />
 
               {/* Seconds input */}
-              <Row>
-                <InputGroup>
-                  <FormControl
-                    placeholder="Max 20 seconds"
-                    aria-label="time"
-                    aria-describedby="basic-addon2"
-                  />
-                  <InputGroup.Text>seconds</InputGroup.Text>
-              </InputGroup>
-              </Row>
+              <InputRange
+              name="A"
+              step={.5}
+              maxValue={20}
+              value={shotTimeA}
+              onChange={value => setShotTimeA(value)} />
+              <h6>Time to Shoot (seconds)</h6>
 
               <br />
               {/* Submit button*/}
@@ -168,7 +240,7 @@ function DelayPage() {
           <Col>
           <h1 className="delay-text">VS</h1>
           {(mobileB && mobileA) ? <Button variant="success" size="lg" onClick={initMatch}>Start</Button>:<Button variant="success" size="lg" disabled>Start</Button>}
-          <Button variant="secondary" size="lg">Reset</Button>
+          <Button   variant="secondary" size="lg" onClick={handleReset}>Reset</Button>
 
           <br />
           <br />
@@ -187,22 +259,28 @@ function DelayPage() {
 
           {/* Side B Column */}
 
-          {currentPlayer==='mobileB' ?<Spinner animation="grow" variant="danger" />: <></>}
           <Col>
           <h1 className="delay-text">Side B</h1>
 
           <Jumbotron style={{backgroundColor:"#1c1f24"}}>
-          <h1><span className="delay-text">0</span></h1>
+          {currentPlayer==='mobileB' ?<Spinner animation="grow" variant="danger" />: <></>}
 
-          <DropdownButton id="dropdown-2" title={mobileB ? mobileB : "Choose Mobile"}>
-            {mobiles.map((mobile,idx) => {
-              return(
-              <>
+          <h1><span className="delay-text">{delayB}</span></h1>
+
+          {(mobileB && mobileA) ?
+            <DropdownButton id="dropdown-1" title={mobileB ? mobileB : "Choose Mobile"}disabled>
+            {mobiles.map((mobile,idx) => (
               <Dropdown.Item key={idx} as="button" onClick={() =>setB(mobile.name)}>{mobile.name}</Dropdown.Item>
-              </>
-            )}
-          )}
-          </DropdownButton>
+
+          ))}
+          </DropdownButton> :
+          <DropdownButton id="dropdown-1" title={mobileB ? mobileB : "Choose Mobile"}>
+          {mobiles.map((mobile,idx) => (
+            <Dropdown.Item key={idx} as="button" onClick={() =>setB(mobile.name)}>{mobile.name}</Dropdown.Item>
+
+        ))}
+        </DropdownButton>
+          }
 
           {mobileB ?
           <Figure>
@@ -220,17 +298,15 @@ function DelayPage() {
 
 
           <Row md={3} sm={3} lg={3}>
-          {[{ name: '1', value: '1' ,variant:"primary"},
-          { name: '2', value: '2',variant:"primary" },
-          { name: 'SS', value: '3' ,variant:"warning"}].map((data,idx) => (
-              <Col>
+          {shotTypes.map((data,idx) => (
+              <Col key={idx}>
                   <ToggleButton
-                  key={idx}
                   type="radio"
                   variant={data.variant}
                   value={data.value}
                   checked={shotValueB===data.value}
-                  onChange={(e) => setshotValueB(e.currentTarget.value)}>
+                  onChange={(e) => setshotValueB(e.currentTarget.value)}
+                  disabled={data.value ==='3' ? true : false}>
                   <br/>
                   {data.name}
                   <br/>
@@ -241,34 +317,35 @@ function DelayPage() {
           </Row>
 
               <br/>
-              <Row>
-                {[0,1,2].map((data,idx) => {
-                  return(
-                  <>
-                  <Col
-                  key={idx}>
-                  <Button variant="danger">
-                  <h6>+600</h6>
-                    <Image src={dual} rounded/>
-                  </Button>
-                  </Col>
-                  </>
-                )}
-              )}
-              </Row>
+                {/* Side B Items*/}
+                <Row><Col>{itemCountB} left</Col></Row>
+                <Row>
+                    <Col>
+                    {itemCountB === 0 || itemClickB===true || shotValueB==='3' ?
+                    <Button variant="danger" disabled>
+                    <h6>+600</h6>
+                      <Image src={dual} rounded/>
+                    </Button> :
+
+                    <Button variant="danger" onClick={toggleItemB}>
+                    <h6>+600</h6>
+                      <Image src={dual} rounded/>
+                    </Button>
+                  }
+                    </Col>
+
+
+                </Row>
 
           <br />
+          {/*Side B Time Input*/}
+          <InputRange
+          step={.5}
+          maxValue={20}
+          value={shotTimeB}
+          onChange={value => setShotTimeB(value)} />
+          <h6>Time to Shoot (seconds)</h6>
 
-          <Row>
-            <InputGroup>
-              <FormControl
-                placeholder="Max 20 seconds"
-                aria-label="time"
-                aria-describedby="basic-addon2"
-              />
-              <InputGroup.Text>seconds</InputGroup.Text>
-          </InputGroup>
-          </Row>
 
           <br />
 
@@ -285,6 +362,7 @@ function DelayPage() {
         </Container>
 
         <Container>
+        <h2 className="justify-content-center">Delay Reference</h2>
         <Table striped bordered hover variant="dark">
           <thead>
             <tr>
@@ -295,24 +373,15 @@ function DelayPage() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Armor</td>
-              <td>111</td>
-              <td>222</td>
-              <td>333</td>
-            </tr>
-            <tr>
-              <td>Mage</td>
-              <td>111</td>
-              <td>222</td>
-              <td>333</td>
-            </tr>
-            <tr>
-              <td>Aduka</td>
-              <td>111</td>
-              <td>222</td>
-              <td>333</td>
-            </tr>
+            {Object.keys(delayData).map((mobile,idx) => (
+              <tr
+              key={idx}>
+              <td>{mobile}</td>
+              <td>{delayData[mobile]["shot1"]}</td>
+              <td>{delayData[mobile]["shot2"]}</td>
+              <td>{delayData[mobile]["ss"]}</td>
+              </tr>
+            ))}
           </tbody>
         </Table>
         </Container>
